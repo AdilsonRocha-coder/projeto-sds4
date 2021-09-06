6436aceec5d059fad0fa5c696e375d6bb50a875c
Adilson Rocha <piuwave@hotmail.com>
2021-09-06 13:26:18 -0300

    Project created

---
+++ backend/src/main/java/com/piuwave/piuvendas/PiuvendasApplication.java
@@ -0,0 +1,13 @@
+package com.piuwave.piuvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiuvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiuvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/piuwave/piuvendas/PiuvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piuwave.piuvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiuvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
