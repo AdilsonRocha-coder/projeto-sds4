46008d58f4e709068bd746d1b9bf0c750464a60b
Adilson Rocha <piuwave@hotmail.com>
2021-09-10 11:29:40 -0300

    Routes

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,23 @@
+//aqui criamos as rotas para a Home e a Dashboard
+
+import Dashboard from "pages/Dashboard";
+import Home from "pages/Home";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+const Routes = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default Routes;
