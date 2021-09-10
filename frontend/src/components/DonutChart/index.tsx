20e1c7782356cd8eeaf2fb30a261c39328b193c5
Adilson Rocha <piuwave@hotmail.com>
2021-09-10 15:08:21 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -29,6 +29,7 @@ const DonutChart = () => {
   }, []);
 
   /*
+  obs: o mockData foi feito apenas para testarmos o endpoint antes dos dados reais
   const mockData = {
     series: [477138, 499928, 444867, 220426, 473088],
     labels: ["Anakin", "Barry Allen", "Kal-El", "Logan", "Padmé"],
